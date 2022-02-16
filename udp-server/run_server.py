b553037da226349c2b2626a96af54f1adc3d4cfc
Timur <thetima2@gmail.com>
2022-02-16 18:21:12 +0700

    udpServer response

--- udp-server/run_server.py
+++ udp-server/run_server.py
@@ -5,4 +5,4 @@ import sys
 if __name__ == '__main__':
     args = sys.argv
     server = UdpServer(args[1], int(args[2]))
-    server.listen()
+    server.start()
--- udp-server/udpServer.py
+++ udp-server/udpServer.py
@@ -1,4 +1,5 @@
 import socket
+import sys
 
 
 class UdpServer:
@@ -7,12 +8,18 @@ class UdpServer:
         self.local_port = local_port
         self.buffer_size = 1024
         self.server_socket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+        self.server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         self.server_socket.bind((self.local_ip, self.local_port))
 
-    def listen(self):
-        while True:
-            bytes_address_pair = self.server_socket.recvfrom(self.buffer_size)
-            message = bytes_address_pair[0]
-            address = bytes_address_pair[1]
-            print(f"Message is {message} and (IP, port) is {address}")
+    def start(self):
+        try:
+            while True:
+                bytes_address_pair = self.server_socket.recvfrom(self.buffer_size)
+                message = bytes_address_pair[0]
+                address = bytes_address_pair[1]
+                print(f"Message is {message} and (IP, port) is {address}")
+                message = f"{message.decode()}_response".encode()
+                self.server_socket.sendto(message, address)
+        except KeyboardInterrupt:
+            sys.exit()
 
